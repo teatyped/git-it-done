f766abc267079f2e98e6af3b4f4e67daeacceca2
Khai Truong <kminht@gmail.com>
2022-03-30 17:04:05 -0400

    get userinput and use get api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,27 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if(username){
+        getUserRepos(username);
+        nameInputEl.value= "";
+    }
+    else{
+        alert("please enter a GitHub usersname");
+    }
+  };
+  
+
+
 var getUserRepos = function(user) {
     
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
 
     fetch(apiUrl)
         .then(function(response){
@@ -12,4 +32,4 @@ var getUserRepos = function(user) {
         });
   };
   
-  getUserRepos();
+  userFormEl.addEventListener("submit", formSubmitHandler);
