babaf5f66241e180bfcfb40fd0224f305eb2d14e
Khai Truong <kminht@gmail.com>
2022-03-30 16:41:37 -0400

    script updated fectch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,14 @@ var getUserRepos = function() {
     console.log("function was called");
 
     fetch("https://api.github.com/users/octocat/repos")
+        .then(function(response){
+            response.json()
+            .then(function(data){
+                console.log(data);
+            })
+        });
+
+        console.log("outside");
 
 
   };
