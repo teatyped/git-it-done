aa8b1313f48b14afb510c0688d187c162d7932b7
Khai Truong <kminht@gmail.com>
2022-03-30 19:34:47 -0400

    display user repos to dom

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
-
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 
 var formSubmitHandler = function(event) {
@@ -17,6 +18,52 @@ var formSubmitHandler = function(event) {
     }
   };
   
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    //loop over repos
+    for(var i = 0; i < repos.length; i++){
+        // formant repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        
+        // append to container
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //append to container
+        repoEl.appendChild(titleEl);
+        
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+// append to container
+repoEl.appendChild(statusEl);
+
+
+
+        //append container to the dom
+        repoContainerEl.appendChild(repoEl); 
+
+    }
+
+};
+
 
 
 var getUserRepos = function(user) {
@@ -28,6 +75,7 @@ var getUserRepos = function(user) {
             response.json()
             .then(function(data){
                 console.log(data);
+                displayRepos(data, user);
             });
         });
   };
